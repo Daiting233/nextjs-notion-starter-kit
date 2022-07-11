41ba57b105e6b956a7f70573220221a36667ab21
Daiting233 <109056560+Daiting233@users.noreply.github.com>
2022-07-11 15:00:38 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -13,23 +13,6 @@ export default siteConfig({
   domain: 'new-media-collection',
   author: 'Dai Ting',
 
-  // default notion icon and cover images for site-wide consistency (optional)
-  // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
-  defaultPageCover: null,
-  defaultPageCoverPosition: 0.5,
-
-  // whether or not to enable support for LQIP preview images (optional)
-  isPreviewImageSupportEnabled: true,
-
-  // whether or not redis is enabled for caching generated preview images (optional)
-  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
-  // environment variables. see the readme for more info
-  isRedisEnabled: false,
-
-  // map of notion page IDs to URL paths (optional)
-  // any pages defined here will override their default URL paths
-  // example:
   //
   // pageUrlOverrides: {
   //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
